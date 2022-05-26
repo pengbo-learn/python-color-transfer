c4cf7baf442eaa3b04a860596866f1bfc67f0bcc
Alyetama <56323389+Alyetama@users.noreply.github.com>
2022-05-26 02:33:52 -0400

    Add pyproject.toml; add build workflow; update code style to PEP8

--- demo.py
+++ demo.py
@@ -1,26 +1,27 @@
 # -*- coding: utf-8 -*-
+
 import os
 import time
 
 import cv2
 import numpy as np
 
-from src.color_transfer import ColorTransfer, Regrain
+from python_color_transfer.color_transfer import ColorTransfer, Regrain
 
 
 def demo():
     cur_dir = os.path.abspath(os.path.dirname(__file__))
     img_folder = os.path.join(cur_dir, "imgs")
     img_names = [
-        "scotland_house.png", 
-        "house.jpeg", 
-        "fallingwater.png", 
+        "scotland_house.png",
+        "house.jpeg",
+        "fallingwater.png",
         "tower.jpeg",
     ]
     ref_names = [
-        "scotland_plain.png", 
-        "hats.png", 
-        "autumn.jpg", 
+        "scotland_plain.png",
+        "hats.png",
+        "autumn.jpg",
         "sunset.jpg",
     ]
     out_names = [
@@ -36,8 +37,7 @@ def demo():
     PT = ColorTransfer(n=300)
     RG = Regrain()
 
-    for img_path, ref_path, out_path in zip(
-            img_paths, ref_paths, out_paths):
+    for img_path, ref_path, out_path in zip(img_paths, ref_paths, out_paths):
         # read input img
         img_arr_in = cv2.imread(img_path)
         [h, w, c] = img_arr_in.shape
@@ -48,30 +48,28 @@ def demo():
         print(f"{ref_path}: {h}x{w}x{c}")
         # pdf transfer
         t0 = time.time()
-        img_arr_col = PT.pdf_tranfer(
-            img_arr_in=img_arr_in, img_arr_ref=img_arr_ref)
+        img_arr_col = PT.pdf_tranfer(img_arr_in=img_arr_in,
+                                     img_arr_ref=img_arr_ref)
         print(f"pdf transfer time: {time.time() - t0:.2f}s")
         # regrain
         t0 = time.time()
-        img_arr_reg = RG.regrain(
-            img_arr_in=img_arr_in, img_arr_col=img_arr_col)
+        img_arr_reg = RG.regrain(img_arr_in=img_arr_in,
+                                 img_arr_col=img_arr_col)
         print(f"regrain time: {time.time() - t0:.2f}s")
         # mean transfer
         t0 = time.time()
-        img_arr_mt = PT.mean_std_transfer(
-            img_arr_in=img_arr_in, img_arr_ref=img_arr_ref
-        )
+        img_arr_mt = PT.mean_std_transfer(img_arr_in=img_arr_in,
+                                          img_arr_ref=img_arr_ref)
         print(f"mean std transfer time: {time.time() - t0:.2f}s")
         # lab transfer
         t0 = time.time()
-        img_arr_lt = PT.lab_transfer(
-            img_arr_in=img_arr_in, img_arr_ref=img_arr_ref)
+        img_arr_lt = PT.lab_transfer(img_arr_in=img_arr_in,
+                                     img_arr_ref=img_arr_ref)
         print(f"lab mean std transfer time: {time.time() - t0:.2f}s")
         # display
         img_arr_out = np.concatenate(
-            (img_arr_in, img_arr_ref, img_arr_mt, 
-             img_arr_lt, img_arr_reg), axis=1
-        )
+            (img_arr_in, img_arr_ref, img_arr_mt, img_arr_lt, img_arr_reg),
+            axis=1)
         cv2.imwrite(out_path, img_arr_out)
         print(f"save to {out_path}\n")
 
